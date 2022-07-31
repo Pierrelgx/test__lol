fbffb2aa21ba2346d1a51c7e19b32badb7be39cb
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-07-31 22:55:09 +0200

    test

---
+++ script.js
@@ -0,0 +1,45 @@
+let nameError = document.getElementById('name-error');
+let emailError = document.getElementById('email-error');
+let submitError = document.getElementById('submit-error');
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.lenght == 0){
+        nameError.innerHTML = "Le prénom est obligatoire ";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "Prenom [espace] Nom ";
+        return false;
+    }
+    nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.lenght ==0) {
+        emailError.innerHTML = "Le mail est obligatoire"
+        return false;
+    }
+    
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = "Email invalide"
+        return false;
+    }
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+}
+
+function validateForm(){
+    if(!validateName() || !validateEmail()){
+        submitError.style.display = "block";
+        submitError.innerHTML = "⚠️ Un champs n'est pas correct";
+        setTimeout(function(){submitError.style.display = "none"}, 3000);
+        return false;
+    }
+}
+
