fd405877e79639771de428b3fbb822f83f129fd7
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-08-03 12:36:52 +0200

    lol

--- script.js
+++ script.js
@@ -2,31 +2,32 @@ let nameError = document.getElementById('name-error');
 let emailError = document.getElementById('email-error');
 let submitError = document.getElementById('submit-error');
 
-// function validateName(){
-//     var name = document.getElementById('contact-name').value;
 
-//     if(name.lenght == 0){
-//         nameError.innerHTML = "Le prénom est obligatoire ";
-//         return false;
-//     }
-//     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-//         nameError.innerHTML = "⛔️";
-//         return false;
-//     }
-//     nameError.innerHTML = "✅";
-//     return true;
-// }
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.length == 0){
+        nameError.innerHTML = "Le prénom est obligatoire ";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "❌";
+        return false;
+    }
+    nameError.innerHTML = "✅";
+    return true;
+}
 
 function validateEmail(){
     var email = document.getElementById('contact-email').value;
 
-    if(email.lenght ==0) {
+    if(email.length == 0) {
         emailError.innerHTML = "Le mail est obligatoire"
         return false;
     }
     
     if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML = "⛔️"
+        emailError.innerHTML = "❌"
         return false;
     }
     emailError.innerHTML = "✅";
@@ -37,8 +38,8 @@ function validateEmail(){
 function validateForm(){
     if(!validateName() || !validateEmail()){
         submitError.style.display = "block";
-        submitError.innerHTML = "⚠️ Un champs n'est pas correct";
-        setTimeout(function(){submitError.style.display = "none"}, 3000);
+        submitError.innerHTML = "⚠️ Un champ n'est pas correct";
+        setTimeout(function(){submitError.style.display = "none"}, 4000);
         return false;
     }
 }
@@ -46,4 +47,25 @@ function validateForm(){
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+    
+}
+
+function myFunctionFailed() {
+    var popupFailed = document.getElementById("myPopupFailed");
+    popupFailed.classList.toggle("showFailed");
+    
+}
+
+
+function test() {
+    let result;
+    if (validateName() == true && validateEmail() == true) {
+      result = {myFunction}
+    } else {
+      result = {myFunctionFailed}
+    }
+    return result;
   }
+
+
+  
